b8222d71c3256a584b86f2f0d649da7863f5fd7f
mvcbotelho <marcus.itec@gmail.com>
2018-02-12 14:17:22 -0300

    Separando código em diferentes arquivos

--- forca.rb
+++ forca.rb
@@ -1,44 +1,17 @@
-def da_boas_vindas
-  puts "Bem vindo ao jogo da forca"
-  puts "Qual seu nome?"
-  nome = gets.strip
-  puts "\n\n\n"
-  puts "Começando o jogo para você, #{nome}"
-  nome
-end
-
-def escolhe_palavra_secreta
-  puts "Escolhendo palavra secreta"
-  palavra_secreta = "programador"
-  puts "Palavra secreta com #{palavra_secreta.size} letras... Boa sorte!"
-  palavra_secreta
-end
-
-def nao_quer_jogar?
-  puts "Deseja jogar novamente? (S/N)"
-  quero_jogar = gets.strip
-  nao_quero_jogar = quero_jogar.upcase == "N"
-end
-
-def pede_um_chute(erros, chutes)
-  puts "\n\n\n"
-  puts "Erros até agora #{erros}"
-  puts "Chutes até agora #{chutes}"
-  puts "Entre com uma letra ou uma palavra"
-  chute = gets.strip
-  puts "Será que acertou?"
-  chute
-end
+require_relative 'ui'
 
 def joga(nome)
   palavra_secreta = escolhe_palavra_secreta
-
   erros = 0
   chutes = []
   pontos_ate_agora = 0
 
   while erros < 5
     chute = pede_um_chute chutes, erros
+    if chutes.include? chute
+      avisa_chute_efeutado chute
+      next
+    end
     chutes << chute
 
     chutou_uma_letra = chute.size == 1
@@ -46,19 +19,19 @@ def joga(nome)
       letra_procurada = chute[0]
       total_encontrado = palavra_secreta.count letra_procurada
       if total_encontrado == 0
-        puts "Letra não encontrada"
+        avisa_letra_nao_encontrada
         erros += 1
       else
-        puts "Letra encontrada #{total_encontrado} vezes"
+        avisa_letra_encontrada total_encontrado
       end
     else
       acertou = chute == palavra_secreta
       if acertou
-        puts "Parabéns! Acertou!!!"
+        avisa_acertou_palavra
         pontos_ate_agora += 100
         break
       else
-        puts "Que pena errou"
+        avisa_errou_plavra
         pontos_ate_agora -= 30
         erros += 1
       end
@@ -66,15 +39,17 @@ def joga(nome)
 
   end
 
-  puts "Você ganhou #{pontos_ate_agora} pontos"
+  avisa_pontos_ate_agora
 
 end
 
-nome = da_boas_vindas
+def jogo_da_forca
+  nome = da_boas_vindas
 
-loop do
-  joga nome
-  if nao_quer_jogar?
-    break
+  loop do
+    joga nome
+    if nao_quer_jogar?
+      break
+    end
   end
 end
---
+++ ui.rb
@@ -0,0 +1,55 @@
+def da_boas_vindas
+  puts "Bem vindo ao jogo da forca"
+  puts "Qual seu nome?"
+  nome = gets.strip
+  puts "\n\n\n"
+  puts "Começando o jogo para você, #{nome}"
+  nome
+end
+
+def escolhe_palavra_secreta
+  puts "Escolhendo palavra secreta"
+  palavra_secreta = "programador"
+  puts "Palavra secreta com #{palavra_secreta.size} letras... Boa sorte!"
+  palavra_secreta
+end
+
+def nao_quer_jogar?
+  puts "Deseja jogar novamente? (S/N)"
+  quero_jogar = gets.strip
+  nao_quero_jogar = quero_jogar.upcase == "N"
+end
+
+def pede_um_chute(erros, chutes)
+  puts "\n\n\n"
+  puts "Erros até agora #{erros}"
+  puts "Chutes até agora #{chutes}"
+  puts "Entre com uma letra ou uma palavra"
+  chute = gets.strip
+  puts "Será que acertou?"
+  chute
+end
+
+def avisa_chute_efeutado(chute)
+  puts "Você já chutou #{chute}"
+end
+
+def avisa_letra_nao_encontrada
+  puts "Letra não encontrada"
+end
+
+def avisa_letra_encontrada(total_encontrado)
+  puts "Letra encontrada #{total_encontrado} vezes"
+end
+
+def avisa_acertou_palavra
+  puts "Parabéns! Acertou!!!"
+end
+
+def avisa_errou_plavra
+  puts "Que pena errou"
+end
+
+def avisa_pontos_ate_agora(pontos_ate_agora)
+  puts "Você ganhou #{pontos_ate_agora} pontos"
+end
